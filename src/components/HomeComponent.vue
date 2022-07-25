9251330badf1dde631a9c1761c82521e2982ca03
unknown <dani22.24gra@gmail.com>
2022-07-25 14:07:59 -0500

    Update file

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -7,6 +7,11 @@
       </div>
     </div>
     <template v-if="loading ===false">
+      <div class="navigation">
+        <ul class="pagination">
+          <li v-for="pagina in 12" @click="pagination(pagina)" :key="pagina"><a>{{pagina}}</a></li>
+        </ul>
+      </div>
       <CardsComponent v-for="pokemon in pokemonsPaginados" :key="pokemon.id" :pokemon="pokemon"></CardsComponent>
       <div class="navigation">
         <ul class="pagination">
